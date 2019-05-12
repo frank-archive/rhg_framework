056a06061ce150830af2e83c84615698598e837b
Frank <frankli0324@hotmail.com>
2019-05-12 17:58:36 +0800

    implements some logic and mkdir recognizer

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import json
 import judge_utils
+import os
+
+BRUTE = True
+
+
+def exec_python(cmd, pyver='python2'):
+    p = os.popen(pyver+' '+cmd)
+    r = p.read()
+    p.close()
+    return r
+
 
 vuls = json.load(open('vulnerabilities.json', 'r'))
 
+api_base = vuls['api_base']
+questions = judge_utils.get_questions(api_base)
+vuls = vuls['vuls']
+for i in questions:
+    for j in vuls:
+        if not BRUTE and 'vulnerable' in exec_python(j['matcher']):
+            flags = exec_python(j['exploit'])
+            for flag in flags.split('\n'):
+                if judge_utils.submit_flag(api_base, i, flag):
+                    print(f'攻击成功,flag为{flag}') #logger
+                    break #攻击成功
+            
