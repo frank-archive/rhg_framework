79524a645e8203092c763742f188aefd275b6483
Frank <frankli0324@hotmail.com>
2019-05-13 22:57:40 +0800

    add examples

--- fix/CVE_2016_10033.py
+++ fix/CVE_2016_10033.py
@@ -1,1 +1,6 @@
-# 
+from paramiko import SSHClient, AutoAddPolicy
+
+ssh = SSHClient()
+ssh.set_missing_host_key_policy(AutoAddPolicy())
+ssh.exec_command('some command that can fix this vulnerability')
+ssh.close()
