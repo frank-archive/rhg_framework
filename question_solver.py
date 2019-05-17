5138929565015c735d55197b8210592965fae281
Frank <frankli0324@hotmail.com>
2019-05-17 19:09:25 +0800

    match flag with regex instead of line breaks

--- question_solver.py
+++ question_solver.py
@@ -1,6 +1,7 @@
 import logging
 import coloredlogs
 import json
+import re
 import subprocess
 import judge_utils
 import threading
@@ -17,7 +18,7 @@ vuls = config['vuls']
 reset_defend_after_fail = config['reset_env_after_fail_attempt']['defend']
 reset_attack_after_fail = config['reset_env_after_fail_attempt']['attack']
 correct_flags = []
-
+MATCH_FLAG = "flag{.*?}"
 log = logging.getLogger(__name__)
 coloredlogs.install(
     level='DEBUG',
@@ -32,7 +33,7 @@ log.addHandler(logging.FileHandler(
 def exec_python(cmd, json_arg: str, pyver='python3'):
     log.debug(f"执行{cmd}文件")
     process = subprocess.run(
-        [pyver, cmd], input=json_arg.encode(), 
+        [pyver, cmd], input=json_arg.encode(),
         capture_output=True
     )
     return process.stdout.decode(), process.stderr.decode()
@@ -44,14 +45,17 @@ def try_exploit(question, vul):
         flags, exp_log = exec_python(exp, json.dumps(question))
         with open(f'logs/{exp.replace("/", "_")}.log', 'w') as f:
             f.write(exp_log)
-        for flag in flags.split('\n'):
+        for flag in re.findall(MATCH_FLAG, flags):
             if flag == "":
                 continue
             if flag in correct_flags:
                 log.warning(f'已经提交过{flag}')
                 continue
             if judge_utils.submit_flag(api_base, question, flag):
-                log.info(f'题目{judge_utils.describe_question(question)}攻击成功,flag为{flag}')  # logger
+                # logger
+                log.info(
+                    f'题目{judge_utils.describe_question(question)}攻击成功,flag为{flag}'
+                )
                 correct_flags.append(flag)
             else:
                 log.warning(f'{flag[:10]}...答案错误')
@@ -75,7 +79,8 @@ def try_fix(question, vul):
 def solve(i):
     log.debug("题目信息:"+json.dumps(i, indent=4, sort_keys=True))
     for j in vuls.keys():
-        recon_output = exec_python(config['vuls'][j]['matcher'], json.dumps(i))[0]
+        recon_output = exec_python(
+            config['vuls'][j]['matcher'], json.dumps(i))[0]
         if not BRUTE and 'vulnerable' in recon_output:
             log.info('找到了可能的漏洞点')
             if not DEBUG:
@@ -86,6 +91,7 @@ def solve(i):
             else:
                 try_exploit(i, config['vuls'][j])
                 try_fix(i, config['vuls'][j])
+
 
 if __name__ == '__main__':
     solve(json.loads(input()))
